b7d0e844736d14a1c1bf8d68f696ee2812dfc073
Caio Victor <caiovictor31@live.com>
2018-05-22 14:10:43 -0300

    Upload project

---
+++ src/components/dashboard/DashboardInfoWidgets.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="row dashboard-info-widgets">
+    <div class="col-md-6 col-xl-3">
+      <vuestic-widget class="info-widget">
+        <div class="info-widget-inner">
+          <div class="stats">
+            <div class="stats-title">Produção Total da Semana</div>
+            <div class="stats-number">
+              <i class="ion ion-ios-trending-up-outline text-primary stats-icon icon-wide"></i>
+              59
+            </div>
+          </div>
+        </div>
+      </vuestic-widget>
+    </div>
+    <div class="col-md-6 col-xl-3">
+      <vuestic-widget class="info-widget brand-info">
+        <div class="info-widget-inner">
+          <div class="stats">
+            <div class="stats-title">Maior Produção da Semana</div>
+            <div class="stats-number">
+              <i class="ion ion-ios-arrow-round-up stats-icon icon-wide"></i>
+              Feijão
+            </div>
+          </div>
+        </div>
+      </vuestic-widget>
+    </div>
+    <div class="col-md-6 col-xl-3">
+      <vuestic-widget class="info-widget">
+        <div class="info-widget-inner">
+          <div class="stats">
+            <div class="stats-title">Produção Total do Dia</div>
+            <div class="stats-number">
+              <i class="ion ion-ios-cube-outline text-primary stats-icon icon-wide"></i>
+              59
+            </div>
+          </div>
+        </div>
+      </vuestic-widget>
+    </div>
+    <div class="col-md-6 col-xl-3">
+      <vuestic-widget class="info-widget brand-info">
+        <div class="info-widget-inner">
+          <div class="stats">
+            <div class="stats-title">Início da Produção</div>
+            <div class="stats-number">
+              <i class="ion ion-ios-time-outline stats-icon icon-wide"></i>
+              00:00:00
+            </div>
+          </div>
+        </div>
+      </vuestic-widget>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dashboard-info-widgets',
+
+    mounted () {
+      this.$refs.circleProgress.$data.value = 70
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../sass/_variables.scss";
+
+  .stats-number, .stats-title {
+    line-height: 1;
+  }
+
+  .info-widget-inner {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 100%;
+
+    &.has-chart {
+      justify-content: space-between;
+    }
+
+    .stats {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
+  }
+
+  .stats-number {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 2.625rem;
+    margin-bottom: 0.5rem;
+
+    .stats-icon {
+      font-size: 1.5625rem;
+      position: absolute;
+      top: 0.625rem;
+      left: -1.25rem;
+
+      &.icon-wide {
+        left: -1.875rem;
+      }
+    }
+  }
+
+</style>

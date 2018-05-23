70e29c0e70209c20c0d38d66cae1eb161484f74c
Caio Victor <caiovictor31@live.com>
2018-05-23 17:20:50 -0300

    Creating new component - mychart

--- src/components/dashboard/data-visualisation-daily/DataVisualisation.vue
+++ src/components/dashboard/data-visualisation-daily/DataVisualisation.vue
@@ -1,58 +1,16 @@
 <template>
   <div class="data-visualization">
-    <daily-line></daily-line>
     <div class="row">
       <div class="col-md-12">
-        <vuestic-widget>
-          <div class="data-visualisation-daily dashboard-tab">
-            <div class="row">
-              <!-- <div class="col-md-6"> -->
-                <h3 class="center">Produção Diária</h3>
-              <!-- </div> -->
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <div class="chart-container">
-                  <vuestic-chart v-bind:data="lineChartData" type="line"></vuestic-chart>
-                </div>
-              </div>
-            </div>
-          </div>
-        </vuestic-widget>
+       <my-chart title="Produção Diária" type="line" :data="lineChartData"></my-chart>
       </div>
     </div>
     <div class="row">
       <div class="col-md-6">
-        <vuestic-widget>
-          <div class="data-visualisation-daily dashboard-tab">
-            <div class="row">
-              <h4 class="center">Produção Semanal</h4>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <div class="chart-container">
-                  <vuestic-chart v-bind:data="verticalChartData" type="vertical-bar"></vuestic-chart>
-                </div>
-              </div>
-            </div>
-          </div>
-        </vuestic-widget>
+        <my-chart title="Produção Semanal" type="vertical-bar" :data="verticalChartData"></my-chart>
       </div>
       <div class="col-md-6">
-        <vuestic-widget>
-          <div class="data-visualisation-daily dashboard-tab">
-            <div class="row">
-              <h4 class="center">Gráfico de Produção Mensal</h4>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <div class="chart-container">
-                  <vuestic-chart v-bind:data="pieChartData" type="pie"></vuestic-chart>
-                </div>
-              </div>
-            </div>
-          </div>
-        </vuestic-widget>
+        <my-chart title="Produção Mensal" type="pie" :data="pieChartData"></my-chart>
       </div>
     </div>
   </div>
@@ -62,8 +20,12 @@
   import LineChartData from 'data/charts/LineChartData'
   import VerticalBarChartData from 'data/charts/VerticalBarChartData'
   import PieChartData from 'data/charts/PieChartData'
+  import MyChart from './MyChart.vue'
 
   export default {
+    components: {
+      MyChart
+    },
     name: 'data-visualisation',
     data () {
       return {
@@ -80,7 +42,6 @@
   @import "~bootstrap/scss/functions";
   @import "~bootstrap/scss/variables";
   @import "~bootstrap/scss/mixins/breakpoints";
-
   .chart-container {
     padding: 0 2rem;
     height: 24rem;
---
+++ src/components/dashboard/data-visualisation-daily/MyChart.vue
@@ -0,0 +1,44 @@
+<template>
+     <vuestic-widget>
+          <div class="data-visualisation-daily dashboard-tab">
+            <div class="row">
+              <!-- <div class="col-md-6"> -->
+                <h3 class="center">{{title}}</h3>
+              <!-- </div> -->
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="chart-container">
+                  <vuestic-chart v-bind:data="data" :type="type"></vuestic-chart>
+                </div>
+              </div>
+            </div>
+          </div>
+        </vuestic-widget>
+</template>
+
+<script>
+export default {
+  props: ['title', 'type', 'data'],
+  name: 'my-chart'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../sass/_variables.scss";
+  @import "~bootstrap/scss/functions";
+  @import "~bootstrap/scss/variables";
+  @import "~bootstrap/scss/mixins/breakpoints";
+  .chart-container {
+    padding: 0 2rem;
+    height: 24rem;
+  }
+
+  .center {
+    text-align: center;
+    margin: auto;
+    margin-bottom: 20px;
+  }
+
+
+</style>

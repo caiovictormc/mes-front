e8f32f251bed2f220ddf26f830cb3938029548cf
Caio Victor <caiovictor31@live.com>
2018-05-26 15:49:39 -0300

    Creating ansyc function logout

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import router from '../router/index.js'
+// import router from '../router/index.js'
 
 
 const API_URL = 'http://localhost:8000/'
@@ -8,11 +8,16 @@ const REFRESH_URL = API_URL + 'api/auth/token-refresh/'
 // const CHECK_URL = API_URL + 'api/auth/token/'
 
 
+export var user = {
+  authenticated: false
+}
+
 export async function tryLogin (username, password) {
   const resp = await axios
     .post(LOGIN_URL, {
       username: username, password: password})
   localStorage.setItem('id_token', resp.data.token)
+  user.authenticated = true
   return true
 }
 
@@ -32,9 +37,9 @@ export function getAuthHeader () {
   }
 }
 
-export function logout () {
-  localStorage.removeItem('id_token')
-  router.push('login')
+export async function logout () {
+  user.authenticated = false
+  await localStorage.removeItem('id_token')
 }
 
 // export function isLoggedIn () {

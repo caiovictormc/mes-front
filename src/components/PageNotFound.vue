988d6fe9516acaa6fb1703c6217724c19252b3d5
Caio Victor <caiovictor31@live.com>
2018-06-21 22:20:05 -0300

    Create 404 Page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page404">
+    <h2>Página não encontrada! Volte para página inicial</h2>
+    <router-link :to="{ name: 'dashboard' }">
+      <button class="btn btn-warning center">
+        Voltar
+      </button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageNotFound'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../sass/variables';
+  @import '~bootstrap/scss/mixins/breakpoints';
+  @import "~bootstrap/scss/functions";
+  @import '~bootstrap/scss/variables';
+
+  .page404 {
+    margin: auto;
+    text-align: center;
+  }
+
+</style>
